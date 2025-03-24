125e4a6a54a36546a8c24862d17047e6ecf38317
Yasiel <toyasiel@gmail.com>
2025-03-24 14:36:39 -0700

    added second part to the function

--- function_a.py
+++ function_a.py
@@ -3,6 +3,12 @@ def most_common_value(number_list):
     max_frequency = -1
     most_common_value = None
 
+     for num in number_list:
+        if num in frequency_index:
+            frequency_index[num] += 1
+        else:
+            frequency_index[num] = 1
+
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
